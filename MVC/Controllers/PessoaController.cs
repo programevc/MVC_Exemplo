4f5566a62d99feab42ce8947dc2ff1d6d5101f18
programevc <wellyngton.borges@condofy.com.br>
2020-07-24 16:43:49 -0300

    first commit

---
+++ MVC/Controllers/PessoaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pessoa = new PessoaModelView();
+            pessoa.Nome = "Programe";
+            pessoa.SobreNome = "VC";
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public IActionResult Save(PessoaModelView pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return Ok();
+        }
+    }
+}
---
+++ MVC/Models/PessoaModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class PessoaModelView
+    {
+        [Required]
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+    }
+}
